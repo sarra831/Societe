322e0ccafcb4c2f5f81842111efcd17343c23153
Sarra Chamekh <chamekh.sarra@gmail.com>
2022-04-08 02:43:40 +0200

    Agent de securite

--- Societe/Controllers/PerScController.cs
+++ Societe/Controllers/PerScController.cs
@@ -14,16 +14,66 @@ namespace Societe.Controllers
     [ApiController]
     public class PerScController : ControllerBase
     {
-
-        private readonly IConfiguration configuration;
-        private readonly IWebHostEnvironment env;
-
-        public PerScController(IConfiguration configuration, IWebHostEnvironment env)
+        private readonly DataContext dataContext;
+        public PerScController(DataContext context)
         {
-            this.configuration = configuration;
-            this.env = env;
+            this.dataContext = context;
         }
 
+
+        //read
+        [HttpGet]
+        public async Task<JsonResult> Get()
+        {
+
+
+            return new JsonResult(await this.dataContext.PerSc.ToListAsync());
+
+        }
+        // creation
+        [HttpPost]
+        public async Task<JsonResult> Post(PerSc prs)
+        {
+            this.dataContext.PerSc.Add(prs);
+            await this.dataContext.SaveChangesAsync();
+            return new JsonResult("succès!");
+        }
+        //modifications update
+        [HttpPut]
+
+        public async Task<JsonResult> Put(PerSc prs)
+        {
+
+            var agentsc = await this.dataContext.PerSc.FindAsync(prs.Id);
+            if (agentsc == null)
+            {
+                return new JsonResult("Societe non trouvé ");
+            }
+            agentsc.CarteID = agentsc.CarteID;
+            agentsc.Nom_Prenom = agentsc.Nom_Prenom;
+
+            await this.dataContext.SaveChangesAsync();
+
+            return new JsonResult("actualisé");
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<JsonResult> Delete(int Id)
+        {
+
+
+            var agentsc = await this.dataContext.PerSc.FindAsync(Id);
+            if (agentsc == null)
+            {
+                return new JsonResult("Societe non trouvé");
+            }
+
+            dataContext.PerSc.Remove(agentsc);
+            await this.dataContext.SaveChangesAsync();
+
+            return new JsonResult(" Agent Supprimé !");
+        }
 
     }
 }
